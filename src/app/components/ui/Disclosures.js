1d18b744b751b9369e771d00d6b1c5400fd3bd3e
John Gibbons <jvgiv12@gmail.com>
2026-01-17 16:04:41 -0500

    Fixed disclosures

--- src/app/components/ui/Disclosures.js
+++ src/app/components/ui/Disclosures.js
@@ -3,12 +3,7 @@ import React from 'react'
 export default function Disclosures() {
   return (
     <div className='disc-container'>
-      <p>Investment Advisory Services oered through First Financial
-
-Advisory Services, Inc., a Registered Investment Advisor. Securi-
-ties oered through Cetera Advisors, LLC, member FINRA/SIPC.
-
-Cetera is under separate ownership from any other named entity.</p>
+      <p>Investment Advisory Services offered through First Financial Advisory Services, Inc., a Registered Investment Advisor. Securities offered through Cetera Advisors, LLC, member FINRA/SIPC.  Cetera is under separate ownership from any other named entity.</p>
     </div>
   )
 }
